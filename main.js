9b65a1d5e494195835b1f302492823d2fda684a3
Alexander Reyes <alexandereliasr9@gmail.com>
2021-03-09 21:00:07 -0400

    return problem solved

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 const fs = require('fs')
+const fsPromises = require('fs').promises; 
 let MAX_SENTENCE_LENGTH = 207;
 
 
-function findLine(rnc) {
-    var response = "";
-
+function findLine(rnc,fn) {
     fs.readFile('DGII_RNC.txt', 'utf-8', function(err, data){    
         let startPoint = data.search(rnc)
         let extract = data.substring(startPoint, startPoint+MAX_SENTENCE_LENGTH);
         let arrayOfSolution = extract.split("\n")
-        console.log(arrayOfSolution[0])
-        response = arrayOfSolution[0]; 
+        fn(arrayOfSolution[0])
     }); 
-
-    return response;
 }
 
-var ex = findLine("101020164");
+//you shlould call it this way
+findLine("101020164", function(sentence){
+    //In sentence the sentence variable u have the value u want 
+    console.log(sentence);
+  });
 
-console.log(ex)
